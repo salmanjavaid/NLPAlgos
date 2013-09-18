4f9941155f288a932ad9f597800ee6e395b21add
Salman Javaid <javaid.salman@gmail.com>
2013-09-18 13:37:11 +0500

    Lesk Updates Comments added

--- Lesk/Lesk.py
+++ Lesk/Lesk.py
@@ -4,21 +4,31 @@ import nltk
 import re
 
 
+#synset for each word
 
 def ret_synsets(var):
     return wn.synsets(var)
 
+# synset definiton
+
 def ret_definition(var):
     return wn.synset(str(var)).definition
 
+# regular expression to find words between ''
+
 def ret_extract(var):
     return re.findall("\'(.*?)\'", str(var))
+
+# wrapper for synsets
 
 def wrapper_ret_synsets(string):
     list_ = []
     for var in string:
          list_.append(ret_extract(ret_synsets(var)))
     return list_
+
+
+# wrapper for definitions
 
 def wrapper_ret_definition(list_):
     row = -1
@@ -32,6 +42,8 @@ def wrapper_ret_definition(list_):
            col = col + 1
 
     return list_
+
+# This function shoudl be updated and Test_* should be brought in
 
 def Lesk(list_):
 
@@ -50,9 +62,10 @@ def Lesk(list_):
 
     return count_list
 
+# for each word, find the number of definitions
 
 def Test(list_):
-     rows = -1
+     rows = -1 # Because of python's terribly confusing delimitors, starting row with -1 instead of 1
      cols = 0
      columns = []
      for row in list_:
@@ -63,6 +76,9 @@ def Test(list_):
          columns.append(cols)
      return (columns, rows + 1)
 
+
+# remove stopwords
+
 def StopWords(str1):
      words = str1.split()
      for word in words:
@@ -70,6 +86,7 @@ def StopWords(str1):
              words.remove(word)
      return set(words)
 
+# compare the definition here by splitting each word's definitions and then working on them individually
 
 def Test_2(str1, list_):
      
@@ -78,8 +95,10 @@ def Test_2(str1, list_):
           scores.append(len(StopWords(str1).intersection(StopWords(l))))
  #         print str1
  #         print l
-#          print '\n'
+ #         print '\n'
      return scores
+
+# wrapper function to compare the first word definition with each word
 
 def Test_1(list_):
     [columns, rows] = Test(list_)
@@ -87,17 +106,18 @@ def Test_1(list_):
     for k in range(0, 1):
         for i in range(0, columns[k]):
             scores.append(Test_2(str(list_[k][i]), list_[k + 1]))
- #           print '\n'
+    
     return scores    
 
 def main():
-    var = 'pine cone'
-    tokens = var.split(' ')
-#    list_ = wrapper_ret_synsets(tokens)
-    print Test_1(wrapper_ret_definition(wrapper_ret_synsets(tokens)))
+    var = 'pine cone' #Input to Lesk
+    tokens = var.split(' ') #Split the words
+
+    # retrieve synsets of each word, and then definition of each word
+    print Test_1(wrapper_ret_definition(wrapper_ret_synsets(tokens))) 
     
-#    stemmer = SnowballStemmer("english")
-#    print stemmer.stem('saying')
+
+
 
 if __name__ == "__main__":
     main()
